91fb3ed9fe034f24fb28ff69990b0a94c4fc453f
Liam Jackson <ljackson3613@yahoo.com>
2021-03-03 09:30:23 -0600

    prepare.py

--- prepare.py
+++ prepare.py
@@ -92,7 +92,52 @@ def split(df, stratify_by=None):
         train, validate = train_test_split(df, test_size=.3, random_state=123, stratify=train[stratify_by])
     
     return train, validate, test
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Binary output for clf
 
+def get_metrics_binary_clf(clf):
+    '''
+    get_metrics_binary takes in a confusion matrix (cnf) for a binary classifier and prints out metrics based on
+    values in variables named X_train, y_train, and y_pred.
+    
+    return: a classification report as a transposed DataFrame
+    '''
+    accuracy = clf.score(X_train, y_train)
+    class_report = pd.DataFrame(classification_report(y_train, y_pred, output_dict=True)).T
+    conf = confusion_matrix(y_train, y_pred)
+    tpr = conf[1][1] / conf[1].sum()
+    fpr = conf[0][1] / conf[0].sum()
+    tnr = conf[0][0] / conf[0].sum()
+    fnr = conf[1][0] / conf[1].sum()
+    print(f'''
+    The accuracy for our model is {accuracy:.4}
+    The True Positive Rate is {tpr:.3}, The False Positive Rate is {fpr:.3},
+    The True Negative Rate is {tnr:.3}, and the False Negative Rate is {fnr:.3}
+    ''')
+    return class_report
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Binary output for rf
+
+    def get_metrics_binary_rf(rf):
+    '''
+    get_metrics_binary takes in a confusion matrix (cnf) for a binary classifier and prints out metrics based on
+    values in variables named X_train, y_train, and y_pred.
+    
+    return: a classification report as a transposed DataFrame
+    '''
+    accuracy = rf.score(X_train, y_train)
+    class_report = pd.DataFrame(classification_report(y_train, y_pred, output_dict=True)).T
+    conf = confusion_matrix(y_train, y_pred)
+    tpr = conf[1][1] / conf[1].sum()
+    fpr = conf[0][1] / conf[0].sum()
+    tnr = conf[0][0] / conf[0].sum()
+    fnr = conf[1][0] / conf[1].sum()
+    print(f'''
+    The accuracy for our model is {accuracy:.4}
+    The True Positive Rate is {tpr:.3}, The False Positive Rate is {fpr:.3},
+    The True Negative Rate is {tnr:.3}, and the False Negative Rate is {fnr:.3}
+    ''')
+    return class_report
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Idk if I can us this anymore
 
